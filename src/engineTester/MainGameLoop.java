8daa18e207d8a591e06dc2fad24d5a56e3cd1d97
Austin Goodman <agoody77@gmail.com>
2018-04-03 11:50:45 -0500

    Finished basic terrain gen

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,5 +1,9 @@
 package engineTester;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -12,8 +16,9 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
+import renderEngine.EntityRenderer;
 import shaders.StaticShader;
+import terrain.Terrain;
 import textures.ModelTexture;
 
 public class MainGameLoop {
@@ -21,36 +26,44 @@ public class MainGameLoop {
 	public static void main(String[] args) {
 		
 		
-		DisplayManager.createDisplay();
-		
-		Loader loader = new Loader();
-
-
-		
-		RawModel model = OBJLoader.loadObjModel("stall", loader);
-
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-		ModelTexture texture = staticModel.getTexture();
-		texture.setShineDamper(10);
-		texture.setReflectivity(1);
-		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
-		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1,1,1));
-		
-		Camera camera = new Camera();
-		
-		MasterRenderer renderer = new MasterRenderer();
-		
-		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(0, 1, 0);
-			camera.move();
-			renderer.processEntity(entity);
-			renderer.render(light, camera);
-			DisplayManager.updateDisplay();
-		}
-		renderer.cleanUp();
-		loader.cleanUp();
-		DisplayManager.closeDisplay();
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+         
+         
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+         
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("image_rainbow")));
+         
+        List<Entity> entities = new ArrayList<Entity>();
+        Random random = new Random();
+        for(int i=0;i<20;i++){
+            entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
+        }
+         
+        Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
+         
+        Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
+         
+        Camera camera = new Camera();   
+        MasterRenderer renderer = new MasterRenderer();
+         
+        while(!Display.isCloseRequested()){
+            camera.move();
+             
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+            for(Entity entity:entities){
+                renderer.processEntity(entity);
+            }
+            renderer.render(light, camera);
+            DisplayManager.updateDisplay();
+        }
+ 
+        renderer.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+        
 	}
 
 }
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,6 +1,8 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
@@ -9,13 +11,70 @@ public class Camera {
 	private float pitch;
 	private float yaw;
 	private float roll;
+	private float speed;
 	
 	public Camera() {
 		
+		this.speed = 0.5f;
 	
 	}
 	
 	public void move() {
+		
+		yaw =  - (Display.getWidth() - Mouse.getX() / 2);
+		pitch =  (Display.getHeight() / 2) - Mouse.getY();
+		
+		if (pitch >= 90)
+		{
+			
+			pitch = 90;
+			
+		}
+		else if (pitch <= -90)
+		{
+			
+			pitch = -90;
+			
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_W))
+		{
+
+			position.z += -(float)Math.cos(Math.toRadians(yaw)) * speed;
+			position.x += (float)Math.sin(Math.toRadians(yaw)) * speed;
+			
+		}
+		else if (Keyboard.isKeyDown(Keyboard.KEY_S))
+		{
+			position.z -= -(float)Math.cos(Math.toRadians(yaw)) * speed;
+			position.x -= (float)Math.sin(Math.toRadians(yaw)) * speed;
+
+
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_D))
+		{
+			
+			position.z += (float)Math.sin(Math.toRadians(yaw)) * speed;
+			position.x += (float)Math.cos(Math.toRadians(yaw)) * speed;
+
+		}
+		else if (Keyboard.isKeyDown(Keyboard.KEY_A))
+		{
+			
+			position.z -= (float)Math.sin(Math.toRadians(yaw)) * speed;
+			position.x -= (float)Math.cos(Math.toRadians(yaw)) * speed;
+
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_E)) {
+			position.y += speed;
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+			position.y -= speed;
+		}
+		/*
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			position.z-=0.1f;
 		}
@@ -34,6 +93,7 @@ public class Camera {
 		if(Keyboard.isKeyDown(Keyboard.KEY_Q)) {
 			position.y-=0.1f;
 		}
+		*/
 	}
 
 	public Vector3f getPosition() {
